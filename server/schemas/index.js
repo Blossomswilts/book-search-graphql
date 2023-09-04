64ee2e2be90034a18eeaef0aa4446daf5b0ff04d
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-04 16:59:45 -0400

    added typedefs

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// Export TypeDefs and Resolvers for apollo server
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+// Create our typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    input BookInput {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+// Export the typeDefs
