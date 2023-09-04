b46b49e7baa41e123b4844bf1bf620e95518273e
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-04 16:54:24 -0400

    boilerplate server code fot apollo

--- server/server.js
+++ server/server.js
@@ -6,6 +6,21 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Add Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
+//New Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware, //middleware function
+});
+
+//Integrate Apollo Server with middleware
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +29,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => { //catch all route
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    //Shows where server is running
+    console.log(`API server running on port ${PORT}!`);
+    //log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
