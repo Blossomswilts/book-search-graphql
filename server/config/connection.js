da42915729264182d1337fe80a93ec0a6f50c1e2
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-06 08:55:44 -0400

    updated mongo connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb+srv://Blossomswilt:Spineworld2@booksearch.sr4slvp.mongodb.net/?retryWrites=true&w=majority"
+    process.env.MONGODB_URI || "mongodb+srv://Blossomswilt:Spineworld2@cluster0.sr4slvp.mongodb.net/?retryWrites=true&w=majority"
 );
 
 module.exports = mongoose.connection;
