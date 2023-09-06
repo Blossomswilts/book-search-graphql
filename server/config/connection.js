daeca122e993720d8ae8a9fcc889073811940e61
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-06 11:32:24 -0400

    herokue test - removed semi colon

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect(
     process.env.MONGODB_URI ||
         "mongodb+srv://Blossomswilt:Spineworld2@cluster0.sr4slvp.mongodb.net/test?retryWrites=true&w=majority"
-);
+)
 
 module.exports = mongoose.connection;
