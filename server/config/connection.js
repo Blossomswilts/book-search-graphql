b4e31e99a9b954a9cff0f96164973457e68631a5
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-05 23:19:02 -0400

    mongo connect atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/tech-friends"
+    process.env.MONGODB_URI || "mongodb+srv://Blossomswilt:Spineworld2@booksearch.sr4slvp.mongodb.net/?retryWrites=true&w=majority"
 );
 
 module.exports = mongoose.connection;
