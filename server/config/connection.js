17fd01ba24b8b8d9e2990cc6954b9bdff06481c2
Michael Tranquillo <117021869+Blossomswilts@users.noreply.github.com>
2023-09-09 14:46:27 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
     process.env.MONGODB_URI ||
-        "mongodb+srv://Blossomswilt:Spineworld2@cluster0.sr4slvp.mongodb.net/test?retryWrites=true&w=majority"
+        "mongodb+srv://Blossomswilt:password@cluster0.sr4slvp.mongodb.net/test?retryWrites=true&w=majority"
 )
 
 module.exports = mongoose.connection;
